e2ab324485ae96d8b7ea549fcbfb13a1814e206e
redo_oreon <sokolovskii.vlad2011@mail.ru>
2023-09-10 12:07:46 +0300

    add saving tickers in the local storage

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container mx-auto flex flex-col items-center bg-gray-100 p-4">
     <div
-      class="hidden fixed w-100 h-100 opacity-80 bg-purple-800 inset-0 z-50 flex items-center justify-center"
+      class="hidden fixed w-100 h-100 bg-purple-800 inset-0 z-50 flex items-center justify-center"
     >
       <svg
         class="animate-spin -ml-1 mr-3 h-12 w-12 text-white"
@@ -34,7 +34,7 @@
             <div class="mt-1 relative rounded-md shadow-md">
               <input
                 v-model="ticker"
-                v-on:keydown.enter="add"
+                v-on:keydown.enter="add(ticker)"
                 id="wallet"
                 class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
                 name="wallet"
@@ -42,9 +42,7 @@
                 type="text"
               />
             </div>
-            <div
-              class="flex bg-white shadow-md p-1 rounded-md shadow-md flex-wrap"
-            >
+            <div class="flex bg-white shadow-md p-1 rounded-md flex-wrap">
               <span
                 class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer"
               >
@@ -70,7 +68,7 @@
           </div>
         </div>
         <button
-          @click="add"
+          @click="add(ticker)"
           class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
           type="button"
         >
@@ -95,25 +93,25 @@
         <hr class="w-full border-t border-gray-600 my-4" />
         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
           <div
-            v-for="t of tickers"
-            :key="t.name"
-            @click="select(t)"
+            v-for="ticker of tickers"
+            :key="ticker.name"
+            @click="select(ticker)"
             :class="{
-              'border-4': selected === t,
+              'border-4': selected === ticker,
             }"
             class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
           >
             <div class="px-4 py-5 sm:p-6 text-center">
               <dt class="text-sm font-medium text-gray-500 truncate">
-                {{ t.name }} - USD
+                {{ ticker.name }} - USD
               </dt>
               <dd class="mt-1 text-3xl font-semibold text-gray-900">
-                {{ t.price }}
+                {{ ticker.price }}
               </dd>
             </div>
-            <div class="w-full border-t border-gray-200"></div>
+            <div class="w-full border-ticker border-gray-200"></div>
             <button
-              @click.stop="remove(t)"
+              @click.stop="remove(ticker)"
               class="flex items-center justify-center font-medium w-full bg-gray-100 px-4 py-4 sm:px-6 text-md text-gray-500 hover:text-gray-600 hover:bg-gray-200 hover:opacity-20 transition-all focus:outline-none"
             >
               <svg
@@ -183,32 +181,50 @@ export default {
   name: "App",
   data() {
     return {
-      ticker: "BTC",
+      ticker: "",
       tickers: [],
       selected: null,
       graph: [],
     };
   },
+  created() {
+    const tickersData = JSON.parse(localStorage.getItem("TICKERS"));
+    if (tickersData) {
+      this.tickers = tickersData;
+      tickersData.forEach((ticker) => this.subscribeToUpdates(ticker.name));
+    }
+  },
   methods: {
-    add() {
-      const currentTicker = {
-        name: this.ticker,
-        price: "-",
-      };
-
-      this.tickers.push(currentTicker);
+    subscribeToUpdates(tickerName) {
       setInterval(async () => {
         const f = await fetch(
-          `https://min-api.cryptocompare.com/data/price?fsym=${currentTicker.name}&tsyms=USD&api_key=ac5241c9285ad2d72dce91e49717e58404783406f87db1d2fd4a0641a19add51`
+          `https://min-api.cryptocompare.com/data/price?fsym=${tickerName}&tsyms=USD&api_key=b590b10b9658e433d22b9ab95e234b84cda3c4ed9b0ce21a5577b768d8143620`
         );
         const data = await f.json();
-        this.tickers.find((t) => t.name === currentTicker.name).price =
+        this.tickers.find((ticker) => ticker.name === tickerName).price =
           data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
-        if (this.selected?.name === currentTicker.name) {
+        if (this.selected?.name === tickerName) {
           this.graph.push(data.USD);
         }
       }, 1000);
       this.ticker = "";
+    },
+    add(ticker) {
+      if (!ticker) {
+        return;
+      }
+      const currentTicker = {
+        name: ticker.toUpperCase(),
+        price: "-",
+      };
+
+      if (this.tickers.find((t) => t.name === currentTicker.name)) {
+        this.warningIsShown = true;
+        return;
+      }
+      this.tickers.push(currentTicker);
+      localStorage.setItem("TICKERS", JSON.stringify(this.tickers));
+      this.subscribeToUpdates(currentTicker.name);
     },
     remove(ticker) {
       this.tickers = this.tickers.filter((t) => t !== ticker);
@@ -221,7 +237,6 @@ export default {
       );
     },
     select(ticker) {
-      console.log(this);
       this.selected = ticker;
       this.graph = [];
     },
